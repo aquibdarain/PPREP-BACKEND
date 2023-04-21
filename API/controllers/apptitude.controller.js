1ad47961bb57057bd55eb0c7c9eb25bb7bf681c7
Aquib Darain <darainaquib4@gmail.com>
2023-04-21 17:16:38 +0530

    technical apis updated

--- API/controllers/apptitude.controller.js
+++ API/controllers/apptitude.controller.js
@@ -3,3 +3,35 @@ const express = require('express');
 const connection = require('../../config/db.connection');
 let router = express.Router();
 
+
+router.post('/add', (req, res) => {
+    console.log("apptitude", req.body);
+    let technical = req.body;
+    console.log("apptitude", apptitude);
+    var query1 = "insert into technical (question,image,answer) values(?,?,?);";
+    console.log(apptitude.question, apptitude.image, apptitude.answer);
+    connection.query(query1, [apptitude.question, apptitude.image, apptitude.answer], (err, result) => {
+        if (!err) {
+            return res.status(200).json({ message: "apptitude added successfully" + result });
+        }
+        else {
+            console.log("error-", err);
+            return res.status(500).json(err)
+        }
+    })
+})
+
+router.get('/get', (req, res, next) => {
+    var query2 = "select * from apptitude"
+    connection.query(query2, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        }
+        else {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    })
+})
+
+module.exports = router;
--- API/controllers/technical.controller.js
+++ API/controllers/technical.controller.js
@@ -33,4 +33,53 @@ router.get('/get', (req, res, next) => {
     })
 })
 
+// router.get('/getById/:id', (req, res, next) => {
+//     const id = req.params.id;
+//     var query4 = "select id, title,description,image,content from blogs where id=?";
+//     connection.query(query4, [id], (err, results) => {
+//         if (!err) {
+//             return res.status(200).json(results[0]);
+//         }
+//         else {
+//             return res.status(500).json(err)
+//         }
+//     })
+// })
+
+
+router.put('/update/:id',(req, res, next) => {
+    let id = req.params.id
+    let technical = req.body;
+    var query5 = "UPDATE technical SET title=?,description=?,image=?,content=? where id=" + id;
+    connection.query(query5, [technical.question, technical.image, technical.answer], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "blogs id does not found" })
+            }
+            return res.status(200).json({ message: "blogs updated successfully" });
+        }
+        else {
+            return res.status(500).json(err)
+        }
+    })
+})
+
+
+// router.delete('/delete/:id', /*checkRole.checkRole,*/(req, res, next) => {
+//     const id = req.params.id;
+//     var query6 = "delete from blogs where id=?";
+//     connection.query(query6, [id], (err, results) => {
+//         if (!err) {
+//             if (results.affectedRows == 0) {
+//                 return res.status(404).json({ message: "blogs id does not found" });
+//             }
+//             return res.status(200).json({ message: "blogs deleted successfuly" });
+//         }
+//         else {
+//             return res.status(500).json(err)
+//         }
+//     })
+// })
+
+
 module.exports = router;
