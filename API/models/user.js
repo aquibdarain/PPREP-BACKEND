f094652a33928655c881d0177b520f4bfe4d9a1b
Aquib Darain <darainaquib4@gmail.com>
2023-04-21 16:49:23 +0530

    updated till technical

--- API/models/user.js
+++
@@ -1,7 +0,0 @@
-// const connection = require('../../config/db.connection');
-// const express = require('express');
-// let router = express.Router();
-// require('dotenv').config();
-
-// const jwt = require('jsonwebtoken');
-// var checkRole = require('../services/checkRole')
